8ef934530b0ee9eafac4d0fe5592618d51c17501
Sehyun Yun <137497373+nawhji@users.noreply.github.com>
2026-06-03 20:21:50 +0900

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+
+        def is_valid(c):
+            c = c.lower()
+            return (
+                ord("a") <= ord(c) <= ord("z")
+                or ord("0") <= ord(c) <= ord("9")
+            )
+
+        while l < r:
+            if not is_valid(s[r].lower()):
+                r -= 1
+                continue
+            if not is_valid(s[l].lower()):
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
